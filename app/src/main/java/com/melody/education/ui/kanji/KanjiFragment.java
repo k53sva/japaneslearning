f8c0db4a35a17b277d0fe4b8d479a2aab97b0940
MinhPQ <minh.pq@ifs.com.vn>
2016-12-07 11:05:06 +0700

    fix topic

--- app/src/main/java/com/melody/education/ui/kanji/KanjiFragment.java
+++ app/src/main/java/com/melody/education/ui/kanji/KanjiFragment.java
@@ -1,29 +1,16 @@
 package com.melody.education.ui.kanji;
 
 import android.os.Bundle;
-import android.support.design.widget.AppBarLayout;
-import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.melody.education.App;
 import com.melody.education.R;
 import com.melody.education.adapter.ViewPagerAdapter;
-import com.melody.education.model.KanjiContent;
 import com.melody.education.model.KanjiGroup;
 import com.melody.education.ui.BaseFragment;
-import com.melody.education.ui.MainActivity;
-import com.melody.education.ui.kanji.KanjiContentFragment;
-import com.melody.education.ui.kanji.KanjiLevelFragment;
-import com.melody.education.ui.syllabaries.HiraganaFragment;
-import com.melody.education.ui.syllabaries.KatakanaFragment;
-import com.melody.education.utils.DataCache;
 import com.melody.education.utils.DataHelper;
-import com.melody.education.utils.Utils;
-
 import java.util.List;
 
 import rx.Observable;
